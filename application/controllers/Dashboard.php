e727f0e77965ed36e1cdaa74d496690cb0c4b10b
piuru <riswantoaldi789@gmail.com>
2024-06-27 02:06:38 +0700

    tambah logika program

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -7,11 +7,35 @@ class Dashboard extends CI_Controller
     {
         parent::__construct();
         is_login();
+        $this->load->model('User_model');
+        $this->load->model('Absensi_model');
+        $this->load->model('Jam_model', 'jam');
     }
 
     public function index()
     {
-        return $this->template->load('template', 'dashboard');
+        $data['jumlah_pegawai'] = $this->User_model->count_all();
+        $data['jumlah_karyawan'] = $this->User_model->count_by_level('Karyawan');
+        $data['jumlah_manager'] = $this->User_model->count_by_level('Manager');
+
+        $data['jumlah_divisi'] = $this->User_model->count_divisi();
+
+        $data['jam_masuk'] = $this->jam->get_jam_masuk();
+        $data['jam_pulang'] = $this->jam->get_jam_pulang();
+
+        $data['absensi_hari_ini'] = $this->Absensi_model->get_absen_hari_ini();
+        $data['jumlah_karyawan_hadir'] = 0;
+        $data['jumlah_karyawan_tidak_hadir'] = 0;
+
+        foreach ($data['absensi_hari_ini'] as $absen){
+            if (!empty($absen->waktu_masuk)){
+                $data['jumlah_karyawan_hadir']++;
+            } else {
+                $data['jumlah_karyawan_tidak_hadir']++;
+            }
+        }
+
+        return $this->template->load('template', 'dashboard', $data);
     }
 
     public function logout()
